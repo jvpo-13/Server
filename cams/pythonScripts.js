adb98abafb1fe86f986b5c1314ca90aefb71540d
jvpo <jvpomigliooliveira@gmail.com>
2025-04-24 21:24:52 -0300

    Correção camera

--- cams/pythonScripts.js
+++ cams/pythonScripts.js
@@ -3,15 +3,15 @@ const { exec } = require('child_process');
 const local = 'C:/Users/Cooper/Desktop/Server';
 
 // Executa o script Python
-exec(local+'/cams/stream_unificado.py', (error, stdout, stderr) => {
+exec(`python ${local}/cams/stream_unificado.py`, (error, stdout, stderr) => {
   if (error) {
     console.error(`Erro ao executar o script Python: ${error.message}`);
-    exec(local+'/cams/stream_unificado.py');
+    exec(`python ${local}/cams/stream_unificado.py`);
     return;
   }
   if (stderr) {
     console.error(`Erro no script Python: ${stderr}`);
-    exec(local+'/cams/stream_unificado.py');
+    exec(`python ${local}/cams/stream_unificado.py`);
     return;
   }
 
